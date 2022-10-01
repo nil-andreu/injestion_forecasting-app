d6ceca75de05ab3a3b9340944f4ddea8e51a44a2
Nil-Andreu <nilandreug@gmail.com>
2022-10-01 09:51:45 +0200

    [NEW] Running the application

--- main.py
+++ main.py
@@ -9,3 +9,9 @@ def create_app() -> FastAPI:
     )
 
     return current_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    # If we call the file, we run from the main file the app, on port 9000, with reloading on every change.
+    uvicorn.run("main:app", port=9000, reload=True)
