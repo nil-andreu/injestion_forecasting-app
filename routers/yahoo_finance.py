0f94a820a4829862f21ffa0c83d44d0bd26867ec
Nil-Andreu <nilandreug@gmail.com>
2022-10-09 20:39:39 +0200

    [NEW] Cleaning Prints

--- routers/yahoo_finance.py
+++ routers/yahoo_finance.py
@@ -2,8 +2,11 @@ import json
 import asyncio
 import requests
 import pandas as pd
-from typing import Dict
+from typing import List
+
 from fastapi import APIRouter, HTTPException, Depends
+
+from db_models.sp500.company import Company
 
 yahoo_finance_router = APIRouter(
     prefix="/yahoo_finance",
@@ -11,32 +14,22 @@ yahoo_finance_router = APIRouter(
     # TODO: dependencies for auth definition
 )
 
+    
+
 @yahoo_finance_router.get("/sp500")
-async def get_list_sp500_companies() -> Dict:
-    """
-    Returns: 
-    - "Symbol"
-    - "SEC filings"
-    - "GICS Sector"
-    - "GICS Sub-Industry"
-    - "Headquarters Location"
-    - "Date first added"
-    - "Founded"
-    """
+async def get_list_sp500_companies() -> List[Company]:
     wiki_url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
 
     # TODO: Try-except for this request, and define fastapi error
     loop = asyncio.get_event_loop()
     future = loop.run_in_executor(None, requests.get, wiki_url)
     res = await future
-    # res = requests.get(wiki_url)
 
     sp_companies = pd.read_html(
         res.text, 
         header=0, 
         )[0]
     
-    print(sp_companies)
     sp_companies = sp_companies.drop(["Security", "CIK"], axis=1)
     sp_companies = sp_companies.fillna('')
     
