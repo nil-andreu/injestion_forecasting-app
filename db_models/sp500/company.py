15780cc14222f910e7d9b945419405b2ea88172f
Nil-Andreu <nilandreug@gmail.com>
2022-10-09 20:28:45 +0200

    [NEW] Definition Company Model

---
+++ db_models/sp500/company.py
@@ -0,0 +1,12 @@
+from date import date
+from pydantic import BaseModel
+
+
+class Company(BaseModel):
+    symbol: str
+    sec_filings: str
+    gics_sector: str
+    gics_sub_sector: str
+    headquarters_location: str
+    date_first_added: date
+    founded: int
