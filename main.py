aec5bca0e2f0dd00accbd315c7d221addc0e9c11
Nil-Andreu <nilandreug@gmail.com>
2022-10-09 20:29:33 +0200

    [NEW] Type hinting Company

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import requests
+import asyncio
 
 import pandas as pd
 import uvicorn as uvicorn
 from fastapi import FastAPI
 
+from routers.yahoo_finance import yahoo_finance_router
 from proto import sp500_pb2_grpc, sp500_pb2
+# TODO: Async routers
+# from kafka.kafka_consumer import consume 
 
+# Get the loop of the actual event
+loop = asyncio.get_event_loop()
 
+# Create the application
 def create_app() -> FastAPI:
     current_app = FastAPI(
         title="Injestion Service FastAPI",
@@ -17,9 +24,18 @@ def create_app() -> FastAPI:
     return current_app
 
 app = create_app()
+app.include_router(yahoo_finance_router)
+
+# And now we start the consumer on the event of the application
+# @app.on_event("startup")
+# async def startup_event():
+
+#     # We create a loop of the consume worker of kafka
+#     loop.create_task(consume())
+
 
 # Define the gRPC service
-class UserListSP(sp500_pb2_grpc.ListSearchSPService):
+class UserListSP(sp500_pb2_grpc.ListSearchSPServicer):
     # When compiling, it will create a Service 
     def ListSP(self, request, context):
         # We obtain the list of the companies from wiki
