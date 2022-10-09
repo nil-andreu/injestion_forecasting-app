1c20231ffd70a4c98e8e6fd4e88241a4b2ce7922
Nil-Andreu <nilandreug@gmail.com>
2022-10-09 10:16:38 +0200

    [NEW] Yahoo finance router

--- routers/yahoo_finance.py
+++ routers/yahoo_finance.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter
+import json
+import requests
+import pandas as pd
+from fastapi import APIRouter, HTTPException, Depends
 
 yahoo_finance_router = APIRouter(
     prefix="/yahoo_finance",
@@ -6,3 +9,19 @@ yahoo_finance_router = APIRouter(
     # TODO: dependencies for auth definition
 )
 
+@yahoo_finance_router.get("/sp500")
+async def get_list_sp500_companies():
+    wiki_url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
+
+    # TODO: Try-except for this request, and define fastapi error
+    res = requests.get(wiki_url)
+
+    sp_companies = pd.read_html(
+        res.text, 
+        header=0, 
+        )[0]
+    
+    sp_companies = sp_companies.drop(["Security", "CIK"], axis=1)
+    
+    
+    return json.dumps(sp_companies.to_dict("record"))
