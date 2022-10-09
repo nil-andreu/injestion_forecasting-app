8061e94ebb7b99c7e4ef591892b8a7e424fc9568
Nil-Andreu <nilandreug@gmail.com>
2022-10-09 20:23:59 +0200

    [NEW] Async definition of routers

--- routers/yahoo_finance.py
+++ routers/yahoo_finance.py
@@ -1,6 +1,8 @@
 import json
+import asyncio
 import requests
 import pandas as pd
+from typing import Dict
 from fastapi import APIRouter, HTTPException, Depends
 
 yahoo_finance_router = APIRouter(
@@ -10,17 +12,31 @@ yahoo_finance_router = APIRouter(
 )
 
 @yahoo_finance_router.get("/sp500")
-async def get_list_sp500_companies():
+async def get_list_sp500_companies() -> Dict:
+    """
+    Returns: 
+    - "Symbol"
+    - "SEC filings"
+    - "GICS Sector"
+    - "GICS Sub-Industry"
+    - "Headquarters Location"
+    - "Date first added"
+    - "Founded"
+    """
     wiki_url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
 
     # TODO: Try-except for this request, and define fastapi error
-    res = requests.get(wiki_url)
+    loop = asyncio.get_event_loop()
+    future = loop.run_in_executor(None, requests.get, wiki_url)
+    res = await future
+    # res = requests.get(wiki_url)
 
     sp_companies = pd.read_html(
         res.text, 
         header=0, 
         )[0]
     
+    print(sp_companies)
     sp_companies = sp_companies.drop(["Security", "CIK"], axis=1)
     sp_companies = sp_companies.fillna('')
     
